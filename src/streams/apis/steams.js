1506b4e13b8233bf3082a546c0b05e850c8f4b3c
Tri Aribowo <t.rahmat.aribowo@gmail.com>
2020-09-17 00:06:26 +0700

    add new url db heroku

--- src/streams/apis/steams.js
+++ src/streams/apis/steams.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://localhost:3001'
-    baseURL: 'http://db-streams.trirahmataribowo.com'
+    // baseURL: 'http://db-streams.trirahmataribowo.com'
+    baseURL: 'https://db-stream.herokuapp.com'
 });
